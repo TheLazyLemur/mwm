3c6f07c751e13d890a694afab0bd99a6fb083f7e
Dan Rousseau <danrousseau@protonmail.com>
2023-06-07 14:14:30 +0200

    test

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"runtime"
 	"time"
 
 	"os/exec"
@@ -14,11 +13,6 @@ import (
 	"github.com/BurntSushi/xgb"
 	"github.com/BurntSushi/xgb/xproto"
 )
-
-func init() {
-	// GLFW event handling must run on the main OS thread
-	runtime.LockOSThread()
-}
 
 func main() {
 	xStuff()
@@ -73,9 +67,9 @@ func initialize(X *xgb.Conn) error {
 	}
 
 	// Flush the request to the X server
+	go drawWindow(X, screen)
 	X.Sync()
 
-	go drawWindow(X, screen)
 	return nil
 }
 
@@ -136,13 +130,6 @@ func drawWindow(X *xgb.Conn, screen *xproto.ScreenInfo) {
 		0, 0, 500, 500, 0,
 		xproto.WindowClassInputOutput, screen.RootVisual, 0, []uint32{})
 
-	// This call to ChangeWindowAttributes could be factored out and
-	// included with the above CreateWindow call, but it is left here for
-	// instructive purposes. It tells X to send us events when the 'structure'
-	// of the window is changed (i.e., when it is resized, mapped, unmapped,
-	// etc.) and when a key press or a key release has been made when the
-	// window has focus.
-	// We also set the 'BackPixel' to white so that the window isn't butt ugly.
 	xproto.ChangeWindowAttributes(X, wid,
 		xproto.CwBackPixel|xproto.CwEventMask,
 		[]uint32{ // values must be in the order defined by the protocol
@@ -151,22 +138,6 @@ func drawWindow(X *xgb.Conn, screen *xproto.ScreenInfo) {
 				xproto.EventMaskKeyPress |
 				xproto.EventMaskKeyRelease})
 
-	// MapWindow makes the window we've created appear on the screen.
-	// We demonstrated the use of a 'checked' request here.
-	// A checked request is a fancy way of saying, "do error handling
-	// synchronously." Namely, if there is a problem with the MapWindow request,
-	// we'll get the error *here*. If we were to do a normal unchecked
-	// request (like the above CreateWindow and ChangeWindowAttributes
-	// requests), then we would only see the error arrive in the main event
-	// loop.
-	//
-	// Typically, checked requests are useful when you need to make sure they
-	// succeed. Since they are synchronous, they incur a round trip cost before
-	// the program can continue, but this is only going to be noticeable if
-	// you're issuing tons of requests in succession.
-	//
-	// Note that requests without replies are by default unchecked while
-	// requests *with* replies are checked by default.
 	err := xproto.MapWindowChecked(X, wid).Check()
 	if err != nil {
 		fmt.Printf("Checked Error for mapping window %d: %s\n", wid, err)
@@ -174,8 +145,6 @@ func drawWindow(X *xgb.Conn, screen *xproto.ScreenInfo) {
 		fmt.Printf("Map window %d successful!\n", wid)
 	}
 
-	// This is an example of an invalid MapWindow request and what an error
-	// looks like.
 	err = xproto.MapWindowChecked(X, 0).Check()
 	if err != nil {
 		fmt.Printf("Checked Error for mapping window 0x1: %s\n", err)
@@ -183,14 +152,7 @@ func drawWindow(X *xgb.Conn, screen *xproto.ScreenInfo) {
 		fmt.Printf("Map window 0x1 successful!\n")
 	}
 
-	// Start the main event loop.
 	for {
-		// WaitForEvent either returns an event or an error and never both.
-		// If both are nil, then something went wrong and the loop should be
-		// halted.
-		//
-		// An error can only be seen here as a response to an unchecked
-		// request.
 		ev, xerr := X.WaitForEvent()
 		if ev == nil && xerr == nil {
 			fmt.Println("Both event and error are nil. Exiting...")
